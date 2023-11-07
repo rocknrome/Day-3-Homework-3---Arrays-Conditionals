4d9f1c304331946264337336da58dc38c4b1fd00
Roman Larionov <pcsaveddotcom@gmail.com>
2023-11-07 14:53:17 -0500

    Easy Going answered

--- answers.js
+++ answers.js
@@ -1,16 +1,25 @@
+/////////////////////////////////
+//Part I: HTML & CSS
+/*Prepare yourself for tomorrow's morning exercise! Watch the following videos on HTML and CSS. You will be working on practicing CSS in your Morning Exercise.
+
+Rank you comfort with the following material on a scale of 0 - 5:
+
+5 - I got this! I am ready to code
+////////////////////////////////
+
+
+////////////////////////////////
+// Part II: JavaScript Reps
+////////////////////////////////*/
+//Write a for loop that will log the numbers 1 through 20.
+
 ////////////////////////////////
 // Easy Going
 ////////////////////////////////
-
-
-////////////////////////////////
-// Get Even
-////////////////////////////////
-
-
-////////////////////////////////
-// Fizz Buzz
-////////////////////////////////
+for (i=1; i<=20; i++) {
+console.log(i);
+}
+console.log("**************************")
 
 
 ////////////////////////////////
